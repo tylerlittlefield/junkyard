7f5655fd5175eabf7d92f788b0b52b8f832a1ec1
tyluRp <tylurp1@gmail.com>
2018-07-05 16:27:37 -0700

    mdb-to-csv.py added to python folder

---
+++ python/mdb-to-csv.py
@@ -0,0 +1,28 @@
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+import urllib2
+import sys
+
+# Register the streaming http handlers with urllib2
+register_openers()
+
+# Use multipart encoding for the input files
+datagen, headers = multipart_encode({ 'files[]': open('/Users/tylerlittlefield/Desktop/Alexandria/data/backup/Alexandria 2018/trees.mdb', 'rb')})
+
+# Create the request object
+request = urllib2.Request('https://www.rebasedata.com/api/v1/convert', datagen, headers)
+
+# Do the request and get the response
+# Here the MDB file gets converted to CSV
+response = urllib2.urlopen(request)
+
+# Check if an error came back
+if response.info().getheader('Content-Type') == 'application/json':
+  print response.read()
+sys.exit(1)
+
+# Write the response to /tmp/output.zip
+with open('/tmp/output.zip', 'wb') as local_file:
+  local_file.write(response.read())
+
+print 'Conversion result successfully written to /tmp/output.zip!' 
