b9b7d66f6b1c17ab030e22c64efeaa35c1817db2
tyluRp <tylurp1@gmail.com>
2018-07-24 15:58:14 -0700

    add credit to string similarity function

--- code/r/levsim.R
+++ code/r/levsim.R
@@ -1,8 +1,10 @@
 # String similarity function
-# https://en.wikipedia.org/wiki/Levenshtein_distance
 #
 # Using this function instead of loading the library because RecordLinkage has
 # a shitload of dependencies.
+#
+# Info: https://en.wikipedia.org/wiki/Levenshtein_distance
+# Credit: https://stackoverflow.com/questions/11535625
 
 levsim = function (str1, str2) {
   x <- RecordLinkage::levenshteinDist(str1, str2)
