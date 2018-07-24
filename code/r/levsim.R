16c315ad6cd297d602b6d5395677096c8f03222d
tyluRp <tylurp1@gmail.com>
2018-07-24 15:56:49 -0700

    added explanation for string similarity function

--- code/r/levsim.R
+++ code/r/levsim.R
@@ -1,5 +1,8 @@
 # String similarity function
 # https://en.wikipedia.org/wiki/Levenshtein_distance
+#
+# Using this function instead of loading the library because RecordLinkage has
+# a shitload of dependencies.
 
 levsim = function (str1, str2) {
   x <- RecordLinkage::levenshteinDist(str1, str2)
